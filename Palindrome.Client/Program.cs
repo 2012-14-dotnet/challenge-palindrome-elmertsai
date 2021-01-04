fdfa4e5e7ff30c76854589063c11a72540c82465
elmer tsai <chenyu.tsai@revature.net>
2021-01-04 11:13:16 -0500

    Functional Program, need testing methods

--- Palindrome.Client/Program.cs
+++ Palindrome.Client/Program.cs
@@ -6,7 +6,71 @@ namespace Palindrome.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            UserView();
+        }
+        static void UserView()
+        {
+            var input = "";
+            bool done = false;
+            Console.WriteLine("Welcome to the Palindrome Checker Challenge!");
+            while(!done)
+            {
+                Console.WriteLine("Please enter a word to check if it's a palindrome or not!");
+                input = Console.ReadLine();
+                if(IsPalindrome(input) == true)
+                {
+                    Console.WriteLine(input + " is a Palindrome!");
+                }
+                else
+                {
+                    Console.WriteLine(input + " is not a Palindrome!");
+                }
+                done = ContinueOrStop();
+            }
+                
+        
+
+        }
+
+        static bool ContinueOrStop()
+        {
+            bool done = false;
+            int input = 0;
+
+            while(!done)
+            { 
+                Console.WriteLine("Enter 1 to continue, 2 to stop. ");
+                int.TryParse(Console.ReadLine(), out input);
+                if(input == 1)
+                {
+                    return false;
+                }
+                else if(input ==2)
+                {
+                    Console.WriteLine("Exiting Program...");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid choice (number 1 or 2)");
+                }
+            }
+            Console.WriteLine("Something went wrong in ContinueOrStop method");
+            return true;
+        }
+        static bool IsPalindrome(string s)
+        {
+            int length = s.Length;
+            s = s.ToLower();
+            for(int i=0; i<length/2; i++)
+            {
+                if(s[i] != s[length-i-1])
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
+    
 }
